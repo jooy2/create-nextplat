100a9410d2212d11dc52179cb6832a60b0aefc2d
leejooy96 <leejooy96@gmail.com>
2022-11-07 09:32:14 +0900

    fix bug, add prettier, add .editorconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,15 @@
 module.exports = {
-    'env': {
-        'es2021': true,
-        'node': true
-    },
-    'extends': [
-        'airbnb-base'
-    ],
-    'parserOptions': {
-        'ecmaVersion': 12
-    },
-    'rules': {
-        'linebreak-style': 0,
-        'import/extensions': 0,
-        'no-console': 0
-    }
-}
+	env: {
+		es2021: true,
+		node: true
+	},
+	extends: ['airbnb-base', 'prettier'],
+	parserOptions: {
+		ecmaVersion: 12
+	},
+	rules: {
+		'linebreak-style': 0,
+		'import/extensions': 0,
+		'no-console': 0
+	}
+};
--- index.js
+++ index.js
@@ -6,51 +6,73 @@ import { join } from 'path';
 const packageName = 'nextplat';
 
 async function run() {
-  const clone = async () => new Promise((resolve, reject) => {
-    try {
-      spawn('git', ['clone', `https://github.com/jooy2/${packageName}`, packageName, '--depth', '1'], { stdio: 'inherit' })
-        .on('error', (err) => {
-          reject(err);
-        })
-        .on('exit', (code) => {
-          resolve(code);
-        });
-    } catch (e) {
-      reject(e);
-    }
-  });
+	const clone = async () =>
+		new Promise((resolve, reject) => {
+			try {
+				spawn(
+					'git',
+					['clone', `https://github.com/jooy2/${packageName}`, packageName, '--depth', '1'],
+					{ stdio: 'inherit' }
+				)
+					.on('error', (err) => {
+						reject(err);
+					})
+					.on('exit', (code) => {
+						resolve(code);
+					});
+			} catch (e) {
+				reject(e);
+			}
+		});
 
-  const runInitScript = async () => new Promise((resolve, reject) => {
-    try {
-      spawn('node', [`${packageName}/.scripts/initialize.js`], { stdio: 'inherit' })
-        .on('error', (err) => {
-          reject(err);
-        })
-        .on('exit', (code) => {
-          resolve(code);
-        });
-    } catch (e) {
-      reject(e);
-    }
-  });
+	const installModules = async () =>
+		new Promise((resolve, reject) => {
+			try {
+				spawn('npm', ['install'], { stdio: 'inherit', cwd: join(process.cwd(), packageName) })
+					.on('error', (err) => {
+						reject(err);
+					})
+					.on('exit', (code) => {
+						resolve(code);
+					});
+			} catch (e) {
+				reject(e);
+			}
+		});
 
-  try {
-    if (existsSync(packageName) && statSync(packageName).isDirectory()) {
-      console.error(`Failed: Directory "${packageName}" already exists.`);
-      process.exit(1);
-      return;
-    }
+	const runInitScript = async () =>
+		new Promise((resolve, reject) => {
+			try {
+				spawn('node', [`${packageName}/.scripts/initialize.js`], { stdio: 'inherit' })
+					.on('error', (err) => {
+						reject(err);
+					})
+					.on('exit', (code) => {
+						resolve(code);
+					});
+			} catch (e) {
+				reject(e);
+			}
+		});
 
-    await clone();
+	try {
+		if (existsSync(packageName) && statSync(packageName).isDirectory()) {
+			console.error(`Failed: Directory "${packageName}" already exists.`);
+			process.exit(1);
+			return;
+		}
 
-    // Remove .git, docs directory
-    rmSync(join(process.cwd(), packageName, '.git'), { recursive: true, force: true });
+		await clone();
 
-    await runInitScript();
-  } catch (e) {
-    console.error(e?.message);
-    process.exit(1);
-  }
+		// Remove .git, docs directory
+		rmSync(join(process.cwd(), packageName, '.git'), { recursive: true, force: true });
+
+		await installModules();
+		await runInitScript();
+	} catch (e) {
+		console.error(e?.message);
+		process.exit(1);
+	}
 }
 
 run();
